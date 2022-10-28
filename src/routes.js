799d0703485bdf7c82d13c6df6b3864c6f9cde74
Maria Luiza Vidal <marialuizavidal01@gmail.com>
2022-10-28 18:25:44 -0300

    inserting src folders

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,19 @@
 import { Router } from 'express';
 
+import authMiddleware from './app/middlewares/auth';
+
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+
 const routes = new Router();
 
-routes.get('/teste', (req, res) => res.json({ ok: true }));
+routes.post('/users', UserController.store);
+
+routes.post('/sessions', SessionController.store);
+
+// todas as rotas abaixo desse middleware precisam estar autenticadas
+routes.use(authMiddleware);
+
+routes.put('/users', UserController.update);
 
 export default routes;
